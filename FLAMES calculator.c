f2686d1c94beed46a712802d4d9f6f3247ec960e
PRAVEEN S <praveenjayidhaselvam123@gmail.com>
2024-06-04 15:13:16 +0530

    Update FLAMES calculator.c

--- FLAMES calculator.c
+++ FLAMES calculator.c
@@ -1,103 +1,76 @@
-#include<stdio.h>
-#include<stdlib.h> 
+#include <stdio.h>
+#include <string.h>
 
-void calculateFlames(char sign1, char sign2) {
- int flames = 0;
+// Function to remove a character from a string
+void removeChar(char *str, int index) {
+    memmove(str + index, str + index + 1, strlen(str) - index);
+}
 
- switch (sign1) {
- case 'A':
- switch (sign2) {
- case 'A':
- flames = 4;
- break;
- case 'B':
- flames = 3;
- break;
- case 'C':
- flames = 2;
- break;
- case 'D':
- flames = 1;
- break;
- case 'E':
- flames = 5;
- break;
- case 'F':
- flames = 6;
- break;
- case 'G':
- flames = 7;
- break;
- case 'H':
- flames = 8;
- break;
- case 'I':
- flames = 9;
- break;
- case 'J':
- flames = 10;
- break;
- case 'K':
- flames = 11;
- break;
- case 'L':
- flames = 12;
- break;
- case 'M':
- flames = 13;
- break;
- case 'N':
- flames = 14;
- break;
- case 'O':
- flames = 15;
- break;
- case 'P':
- flames = 16;
- break;
- case 'Q':
- flames = 17;
- break;
- case 'R':
- flames = 18;
- break;
- case 'S':
- flames = 19;
- break;
- case 'T':
- flames = 20;
- break;
- case 'U':
- flames = 21;
- break;
- case 'V':
- flames = 22;
- break;
- case 'W':
- flames = 23;
- break;
- case 'X':
- flames = 24;
- break;
- case 'Y':
- flames = 25;
- break;
- case 'Z':
- flames = 26;
- break;
- default:
- printf("Invalid sign!\n");
- return;
- }
+// Function to calculate FLAMES
+char calculateFLAMES(char *name1, char *name2) {
+    int len1 = strlen(name1);
+    int len2 = strlen(name2);
 
- printf("Flames compatibility: %d\n", flames);
+    // Copy name2 to a temporary string
+    char temp[len2 + 1];
+    strcpy(temp, name2);
+
+    // Iterate through name1 and remove common letters from name2
+    for (int i = 0; i < len1; i++) {
+        for (int j = 0; j < len2; j++) {
+            if (name1[i] == temp[j]) {
+                removeChar(temp, j);
+                len2--;
+                break;
+            }
+        }
+    }
+
+    // Calculate length of remaining letters
+    int remaining = len1 + len2;
+
+    // Finding the FLAMES result
+    char flames[] = "FLAMES";
+    int index = 0;
+    while (strlen(flames) > 1) {
+        index = (index + remaining - 1) % strlen(flames);
+        removeChar(flames, index);
+    }
+
+    return flames[0];
 }
 
 int main() {
- char sign1 = 'A';
- char sign2 = 'B';
+    char name1[50], name2[50];
+    printf("Enter the first name: ");
+    scanf("%s", name1);
+    printf("Enter the second name: ");
+    scanf("%s", name2);
 
- calculateFlames(sign1, sign2);
+    char result = calculateFLAMES(name1, name2);
 
- return 0;
+    switch (result) {
+        case 'F':
+            printf("Friends\n");
+            break;
+        case 'L':
+            printf("Lovers\n");
+            break;
+        case 'A':
+            printf("Affectionate\n");
+            break;
+        case 'M':
+            printf("Marriage\n");
+            break;
+        case 'E':
+            printf("Enemies\n");
+            break;
+        case 'S':
+            printf("Siblings\n");
+            break;
+        default:
+            printf("Error\n");
+    }
+
+    return 0;
 }
